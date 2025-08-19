c7932e9191ba0f0737ba14787b1daca306e0cbc6
Aakash Kashyap <Aakash.cse@acem.edu.in>
2025-08-19 17:58:18 +0530

    Core Error Solving

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 app.use(bodyParser.json());
 const corsOptions = {
-  origin: ["https://95e00253a924.ngrok-free.app","http://localhost:5173"], 
+  origin: true, 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"],
   credentials: true,            
