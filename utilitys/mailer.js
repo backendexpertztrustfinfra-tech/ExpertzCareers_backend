16a9341215c0b2417da5834ae27d6f6d02285498
Aakash Kashyap <backend.expertztrustfinfra@gmail.com>
2025-09-24 17:44:41 +0530

    changing bravo smtp for gamil sending otp

--- utilitys/mailer.js
+++ utilitys/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT) || 587,
-    secure: process.env.SMTP_SECURE === 'true', // true for 465
+    port: Number(process.env.SMTP_PORT),
+    secure: false, // true for 465
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
